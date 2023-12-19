813e05736c482b63a8688c65b58fc60151c0da90
Iulia Alexandra Ignat <iuliaalexandra.ignat@gmail.com>
2023-12-19 13:21:42 +0200

    add project

---
+++ src/components/pages/Services.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../index.css';
+
+export default function Services() {
+  return <h1 className="services">SERVICES</h1>;
+}
---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../index.css';
+
+export default function SignUp() {
+  return <h1 className="sign-up">SIGN UP</h1>;
+}
